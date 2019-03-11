e4245d0ec2a79c1ab4b2c614919d5d09af8025cd
“尹志达 <yinzhida@qiyi.com>
2019-03-11 16:13:27 +0800

    remove mismatched marker plugin

--- index.js
+++ index.js
@@ -2,14 +2,13 @@
  * Created Date: 2018-10-17 4:38:41
  * Author: yinzhida Email: yinzhida@qiyi.com
  * -----
- * Last Modified: 2019-03-11 15:26:13
+ * Last Modified: 2019-03-11 16:11:40
  * Modified By: yinzhida yinzhida@qiyi.com
  * -----
  * Copyright (c) 2018 IQIYI
  */
 
-import { Videojs, MarkerPlayer } from './src/js/components'
-import { PlayerMarkerMixin } from './src/js/mixins/playerMarker'
+import { Videojs } from './src/js/components'
 import { PlayerContainerMixin } from './src/js/mixins/playerContainer'
 import loadjs from 'loadjs'
 
@@ -18,19 +17,19 @@ function asyncLoaderFactory (comp, videojsPath, videojsCssPath) {
   videojsCssPath = videojsCssPath || '//vjs.zencdn.net/7.3.0/video-js.min.css'
   return (resolve, reject) => {
     try {
-      loadjs([videojsPath, videojsCssPath], 'videojs');
+      loadjs([videojsPath, videojsCssPath], 'videojs')
       loadjs.ready('videojs', function () {
         /* foo.js & bar.js loaded */
         resolve(comp)
-      });
+      })
     } catch (e) {
-      console.warn(e);
+      console.warn(e)
       reject('videojs加载失败!')
     }
   }
 }
 
-function asyncVideojsCompFactory (compName, comp, videojsPath, videojsCssPath) {
+function asyncVideojsCompFactory (compName = 'vue-custom-videojs', comp, videojsPath, videojsCssPath) {
   return {
     install (Vue) {
       Vue.component(compName, asyncLoaderFactory(comp, videojsPath, videojsCssPath))
@@ -41,15 +40,12 @@ function asyncVideojsCompFactory (compName, comp, videojsPath, videojsCssPath) {
 const VueVideojs = {
   install (Vue) {
     Vue.component('vue-videojs', asyncLoaderFactory(Videojs))
-    Vue.component('marker-player', asyncLoaderFactory(MarkerPlayer))
   }
 }
 
 export {
   VueVideojs,
   Videojs,
-  MarkerPlayer,
-  PlayerMarkerMixin,
   PlayerContainerMixin,
   asyncVideojsCompFactory
 }
--- src/js/components/index.js
+++ src/js/components/index.js
@@ -2,15 +2,13 @@
  * Created Date: 2018-10-23 8:05:06
  * Author: yinzhida Email: yinzhida@qiyi.com
  * -----
- * Last Modified: 2019-03-11 15:44:25
+ * Last Modified: 2019-03-11 16:07:20
  * Modified By: yinzhida yinzhida@qiyi.com
  * -----
  * Copyright (c) 2018 IQIYI
  */
 import Videojs from './Videojs'
-import MarkerPlayer from './MarkerPlayer'
 
 export {
-  Videojs,
-  MarkerPlayer
+  Videojs
 }
